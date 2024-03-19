54f5c4f7699e4059fdc5a62fdcf4ed9bec33de80
seaniy <28254509+seaniy@users.noreply.github.com>
2024-03-19 23:51:12 +0800

    update content

--- itiswhatitis/app/page.js
+++ itiswhatitis/app/page.js
@@ -15,7 +15,10 @@ export default function Home() {
           />
         </div>
         <div className="content text-white z-10 relative bg-gray-800 bg-opacity-35 rounded-lg p-8">
-          <h1 className="text-4xl font-bold mb-8">IT IS WHAT IT IS</h1>
+          <h1 className="text-4xl font-bold mb-8">$ITISWHATITIS</h1>
+          <h2 className="text-2xl font-bold mb-8">
+            LAUNCHING ON 20 MARCH 3PM UTC
+          </h2>
           <div className="links flex flex-col gap-4">
             <Link href="https://google.com">CHART</Link>
             <Link href="https://twitter.com/itiswhatitissol">TWITTER</Link>
